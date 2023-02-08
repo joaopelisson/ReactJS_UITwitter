bd46a50505d250dccc51559ba15ba3c1c8bd6efa
joaopelisson <joaopedro.devbr@gmail.com>
2023-02-08 21:48:35 -0200

    page skeleton

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,14 @@
+interface TweetProps{
+    user: string;
+    children: string;
+    likes?: number;
+}
+export function Tweet(props: TweetProps){
+    return (
+        <div>
+            <strong>{props.user}</strong>
+            <p>{props.children}</p>
+            <button>Like {props.likes ?? 0}</button>
+        </div>
+    )
+}
