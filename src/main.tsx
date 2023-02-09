3b5e573a9b12377845896a7929b761ac5624f8d6
joaopelisson <joaopedro.devbr@gmail.com>
2023-02-08 22:23:22 -0200

    visual structure home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, User } from 'phosphor-react';
+import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from 'phosphor-react';
 
 import './global.css';
 
 import twitterLogo from './assets/logo-twitter.svg';
+import { Tweet } from './components/Tweet';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -50,7 +51,28 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <button className="new-tweet" type="button">Tweet</button>
       </aside>
       <div className="content">
-        content
+        <main className="timeline">
+          <div className="timeline-header">
+            Home
+            <Sparkle />
+          </div>
+          
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img src="https://github.com/joaopelisson.png" alt="João Pelisson" />
+              <textarea id="tweet" placeholder="What's happening?" />
+            </label>
+            <button type="submit">Tweet</button>
+          </form>
+
+          <div className="separator"/>
+
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          
+        </main>
       </div>
     </div>
   </React.StrictMode>,
