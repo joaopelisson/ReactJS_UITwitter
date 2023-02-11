eaf763e981e2cf63fd551cd70e47952b00f946c7
joaopelisson <joaopedro.devbr@gmail.com>
2023-02-10 21:23:58 -0300

    Twitter UI finished

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, KeyboardEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separetor";
 import { Tweet } from "../components/Tweet";
@@ -5,21 +6,44 @@ import { TweetsMocks } from "../mocks/mocksDatas";
 import "./Timeline.css";
 
 export function Timeline() {
+  const [newTweetValue, setNewTweetValue] = useState('');
+  const [tweets, setTweets] = useState(TweetsMocks)
+
+  function createNewTweet(event: FormEvent){
+    event?.preventDefault();
+
+    setTweets([newTweetValue, ...tweets]);
+    setNewTweetValue('');
+  }
+
+  function handleHotkeySubmit(event: KeyboardEvent){
+    if(event.key === 'Enter' && (event.ctrlKey || event.metaKey)){
+      setTweets([newTweetValue, ...tweets]);
+      setNewTweetValue('');
+    }
+  }
+
   return (
     <main className="timeline">
-      <Header title="Home" />
+      <Header title="Home" /> 
 
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/joaopelisson.png" alt="João Pelisson" />
-          <textarea id="tweet" placeholder="What's happening?" />
+          <textarea
+          id="tweet"
+          placeholder="What's happening?"
+          value={newTweetValue}
+          onChange={(event) => setNewTweetValue(event.target.value)}
+          onKeyDown={handleHotkeySubmit}
+          />
         </label>
         <button type="submit">Tweet</button>
       </form>
 
       <Separator />
 
-      {TweetsMocks.map((tweet) => {
+      {tweets.map((tweet) => {
         return <Tweet key={tweet} content={tweet} />;
       })}
     </main>
